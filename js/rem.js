0028cf3bb4957958c712524ec44bb35183b97479
吴晓炜 <359443034@qq.com>
2020-04-26 13:28:38 +0800

    1

--- js/rem.js
+++ js/rem.js
@@ -2,7 +2,7 @@ window.onload=function() {
     function fixRem() {
       var windowWidth = document.documentElement.clientWidth || window.innerWidth || document.body.clientWidth;
       // windowWidth = windowWidth > 750 ? 750 : windowWidth;
-      var rootSize = 100 * (windowWidth / 1366);
+      var rootSize = 100 * (windowWidth / 1920);
       var htmlNode = document.getElementsByTagName("html")[0];
       htmlNode.style.fontSize = rootSize + 'px';
     }
